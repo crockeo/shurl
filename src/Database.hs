4a512dcc00285dc9fe9e3b4812feddda747bc8c2
Crockeo <crockeo@gmail.com>
2014-05-28 23:07:36 -0400

    Got the database to half-work.

--- src/Database.hs
+++ src/Database.hs
@@ -8,16 +8,23 @@ import Database.HDBC
 import Control.Monad
 
 import Data.Text.Lazy (Text, unpack, pack)
+import Data.List (sort)
 
 -- The database connection
 dbConnection :: IO Connection
 dbConnection = connectSqlite3 "./redirects.db"
 
+-- Converting SqlValues into Strings
+toStrings :: [[SqlValue]] -> [[String]]
+toStrings l =
+  map (map (\x -> fromSql x :: String)) l
+
 -- Getting a url from a shurl
 getUrl :: Text -> IO (Maybe Text)
 getUrl shurl = do
   db <- dbConnection
-  res <- quickQuery db ("SELECT * FROM redirects WHERE shurl==" ++ show shurl) []
+  res <- quickQuery' db ("SELECT * FROM redirects WHERE shurl==" ++ show shurl) []
+  commit db
 
   if res == []
     then return Nothing
@@ -27,9 +34,19 @@ getUrl shurl = do
 putUrl :: (Text, Text) -> IO ()
 putUrl (shurl, url) = do
   db <- dbConnection
-  run db ("INSERT INTO redirects values(" ++ show shurl ++ ", " ++ show url ++ ")") []
+  quickQuery' db ("INSERT INTO redirects values(" ++ show shurl ++ ", " ++ show url ++ ")") []
   commit db
 
 -- Generating the next key
 nextKey :: IO Text
-nextKey = return "b"
+nextKey = do
+  db <- dbConnection
+  res <- quickQuery' db ("SELECT * FROM redirects") []
+  commit db
+
+  if res == []
+    then return "a"
+    else let fres = last $ sort $ map (head) $ toStrings res in do
+      if last fres == 'z'
+        then return $ pack $ fres ++ "a"
+        else return $ pack $ (init fres) ++ [(succ $ head fres)]
